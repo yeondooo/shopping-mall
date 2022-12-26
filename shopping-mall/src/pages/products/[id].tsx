ef205c2a4e84f5200aa4f3e09a63ff0ee9c4d453
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-12-27 07:06:38 +0900

    first commit

---
+++ shopping-mall/src/pages/products/[id].tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DetailPage = () => {
+	return <div>상세페이지</div>;
+};
+
+export default DetailPage;
---
+++ shopping-mall/src/pages/products/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductList = () => {
+	return <div>상품목록</div>;
+};
+
+export default ProductList;
