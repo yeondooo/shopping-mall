ab18f4c8a0feb89c100dbe79bb169451dd28571e
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-12-31 22:56:28 +0900

    feat: product type 설정

---
+++ shopping-mall/src/components/product/item.tsx
@@ -0,0 +1,14 @@
+import { Product } from '../../types';
+
+const ProductItem = ({ category, description, image, price, rating, title }: Product) => (
+	<li>
+		<p>{category}</p>
+		<p>{title}</p>
+		<p>{description}</p>
+		<img src={image} />
+		<span>${price}</span>
+		<span>{rating.rate}</span>
+	</li>
+);
+
+export default ProductItem;
