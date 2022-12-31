3a6f199d2b2759fdd4201143f8638fd462fb42f0
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-12-31 22:57:29 +0900

    feat: product page api 연결

--- shopping-mall/src/pages/products/index.tsx
+++ shopping-mall/src/pages/products/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import ProductItem from '../../components/product/item';
+import { fetcher, QueryKeys } from '../../queryClient';
+import { Product } from '../../types';
 
 const ProductList = () => {
-	return <div>상품목록</div>;
+	const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () =>
+		fetcher({
+			method: 'GET',
+			path: '/products',
+		})
+	);
+
+	return (
+		<div>
+			<ul>
+				{data?.map((product) => (
+					<ProductItem {...product} key={product.id} />
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default ProductList;
