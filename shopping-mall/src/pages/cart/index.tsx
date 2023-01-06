0e1426867b3f4607a31913de4d3f0e3f20463f38
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-07 01:23:29 +0900

    feat: 장바구니 페이지 생성

--- shopping-mall/src/pages/cart/index.tsx
+++ shopping-mall/src/pages/cart/index.tsx
@@ -1,7 +1,19 @@
+import { useQuery } from 'react-query';
+import CartList from '../../components/cart';
+import { CartType, GET_CART } from '../../graphql/cart';
+import { graphqlFetcher, QueryKeys } from '../../queryClient';
+
 const Cart = () => {
-	return <div>
-    장바구니
-  </div>;
+	const { data } = useQuery(QueryKeys.CART, () => graphqlFetcher(GET_CART), {
+		staleTime: 0,
+		cacheTime: 1000,
+	});
+
+	const cartItems = Object.values(data || {}) as CartType[];
+
+	if (!cartItems.length) return <div>장바구니가 비었어요</div>;
+
+	return <CartList items={cartItems} />;
 };
 
 export default Cart;
